14c16bcba60cf669fe8b78f2341331da2722a79d
MorganeHuebra <41567005+MorganeHuebra@users.noreply.github.com>
2023-11-08 08:13:50 +0000

    Deploying to gh-pages from master @ c91b30912f926e9740cbf59123ea9dc01a3a9bdb 🚀

--- assets/view_cheatSheet-d8e155ea.js
+++
@@ -1,1 +0,0 @@
-import"./main-c7fad90a.js";import{t as n}from"./translate-a208a274.js";document.addEventListener("DOMContentLoaded",()=>{WA.onInit().then(async()=>{const t=document.getElementById("content"),e=document.getElementById("note");t&&(t.innerText=n("views.cheatSheet.content")),e&&(e.innerText=n("views.cheatSheet.note"))})});
